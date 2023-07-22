db81e73e81d51ea1d6354182fd3b0398a023aff0
Roman <i@zangetsu.ru>
2023-07-22 17:11:12 +0500

    first commit

---
+++ mydocumentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.gb","l":"Decorator"},{"p":"ru.gb","l":"Main"},{"p":"ru.gb","l":"OtherClass"}]
---
+++ src/ru/gb/Main.java
@@ -0,0 +1,22 @@
+package ru.gb;
+
+/**
+ * Основной класс приложения. Здесь мы можем описать
+ * его основное назначение и способы взаимодействия с ним.
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Точка входа в программу. С неё всегда всё начинается.
+         * @param args стандартные аргументы командной строки
+         */
+        int result = OtherClass.add(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.sub(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.mul(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.div(2, 2);
+        System.out.println(Decorator.decorate(result));
+    }
+}
